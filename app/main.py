5db58f79688d4fb312f6f578aeee1bd9db70726c
Heather Bowes <musicalheather@gmail.com>
2020-03-25 13:18:43 -0400

    added docker presentation and added readme notes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Docker Lunch and Learn!"
+    return "Welcome to Docker Lunch and Learn! Happy Birthday to me"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
